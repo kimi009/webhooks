5f9b9b55aebb72568820c660cae6e0167f045d8e
chenfuye <444812002@qq.com>
2020-01-15 09:10:13 +0800

    firstcommit

--- webhook.js
+++ webhook.js
@@ -9,7 +9,7 @@ function sign(body) {
 }
 
 const server = http.createServer((req, res) => {
-  console.log(req.method, req.url)
+  console.log(req, res, req.method, req.url)
   if (req.url === '/webhook') {
     let buffers = []
     req.on('data', buffer => {
