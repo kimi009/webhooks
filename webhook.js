fa5c6df3315cf9ef7bd35bdfa41c4ef3c57958c7
chenfuye <444812002@qq.com>
2020-01-15 08:48:57 +0800

    firstcommit

---
+++ webhook.js
@@ -0,0 +1,33 @@
+const http = require('http')
+const crypto = require('crypto')
+const SECRET = 'Vanvy@11'
+function sign(body) {
+  return crypto
+    .createHmac('sha1', SECRET)
+    .update(body)
+    .digest('hex')
+}
+
+const server = http.createServer((req, res) => {
+  console.log(req.method, req.url)
+  if (req.method !== 'GET' || req.url !== '/webhook/') {
+    res.end('not found')
+  } else {
+    let buffers = []
+    req.on('data', buffer => {
+      buffers.push(buffer)
+    })
+    req.on('end', buffer => {
+      let body = Buffer.concat(buffers)
+      let event = req.headers['x-gitea-event']
+      let sign = req.headers['HTTP_X_GITEA_SIGNATURE']
+      console.log(sign, event, req.headers)
+    })
+    res.setHeader('Content-Type', 'application/json')
+    res.end(JSON.stringify({ ok: true }))
+  }
+})
+
+server.listen(6020, () => {
+  console.log('WebHook服务已经启动')
+})
